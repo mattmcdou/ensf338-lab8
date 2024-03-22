04e928eb734af1a342468140e3281e28ac84016a
Matthew McDougall <144636483+mattmcdou@users.noreply.github.com>
2024-03-22 15:25:59 -0600

    worked for partial submission

---
+++ ex1.py
@@ -0,0 +1,54 @@
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.adjacent = []
+
+
+class Edge:
+    
+    def __init__(self, n1, n2, weight):
+        self.nodeOne = n1
+        self.nodeTwo = n2
+        self.weight = weight
+
+
+class Graph:
+
+    def __init__(self):
+        self.elements = []
+
+    def addNode(self, data):
+        node = Node(data)
+        self.elements.append(node)
+        return node
+
+    def removeNode(self, key):
+        for node in self.elements:
+            if node.data == key:
+                self.elements.remove(node) # this looks stupid but removeNode takes a key so i think i have to do it
+
+    def addEdge(self, n1, n2, weight):
+        edge = Edge(n1, n2, weight)
+        added1 = False
+        added2 = False # for checking successful adding of edge
+        for node in self.elements:
+            if node.data == n1.data:
+                node.adjacent.append(edge)
+                added1 = True
+            elif node.data == n2.data:
+                node.adjacent.append(edge)
+                added2 = True
+        if added1 == False or added2 == False:
+            print("Error adding edge")
+
+    def removeEdge(self, n1, n2):
+        for node in self.elements:
+            for edge in node.adjacent:
+                if edge.nodeOne == n1 and edge.nodeTwo == n2:
+                    node.adjacent.remove(edge)
+
+    def importFromFile(self, file):
+        self = Graph()
+        return NotImplemented
+
