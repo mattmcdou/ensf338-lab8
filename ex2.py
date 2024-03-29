e137b0cbbaffa722b7acbd803be8a21b76a49d91
Cealrglario <kirstencarl.elgario@ucalgary.ca>
2024-03-29 13:42:32 -0700

    Almost done Ex2

--- ex2.py
+++ ex2.py
@@ -1,3 +1,59 @@
+import timeit
+import matplotlib.pyplot as plt
+import heapq
+import math
+
+class QueueNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class SlowQueue:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+
+    def enqueue(self, item):
+        new_node = QueueNode(item)
+        if self.rear is None:
+            self.front = new_node
+            self.rear = new_node
+        else:
+            self.rear.next = new_node
+            self.rear = new_node
+
+    def dequeue(self):
+        if self.is_empty():
+            print("Cannot dequeue from an empty queue")
+        else:
+            dequeued_item = self.front.data
+            self.front = self.front.next
+            if self.front is None:
+                self.rear = None
+            return dequeued_item
+
+    def is_empty(self):
+        return self.front is None
+
+    def peek(self):
+        if not self.is_empty():
+            return self.front.data
+        else:
+            return None
+
+class FastQueue:
+    def __init__(self):
+        self.heap = []
+    
+    def push(self, item, priority):
+        heapq.heappush(self.heap, (priority, item))
+    
+    def pop(self):
+        return heapq.heappop(self.heap)[1]
+    
+    def __len__(self):
+        return len(self.heap)
+
 class Node:
 
     def __init__(self, data):
@@ -93,9 +149,43 @@ class Graph:
             for adj in element.adjacent:
                 print(f"adjacenies: {adj.nodeOne.data}, {adj.nodeTwo.data}, {adj.weight}")
 
+    def slowSP(self, start_node):
+        distances = {node: math.inf for node in self.elements}
+        distances[start_node] = 0
+
+        queue = SlowQueue()
+        queue.enqueue(start_node)
+
+        while not queue.is_empty():
+            current_node = queue.dequeue()
+
+            for edge in current_node.adjacent:
+                adjacent_node = edge.nodeTwo
+                currentDistance = distances[current_node] + edge.weight
+                if currentDistance < distances[adjacent_node]:
+                    distances[adjacent_node] = currentDistance
+                    if adjacent_node not in queue:
+                        queue.enqueue(adjacent_node)
+
+        return distances
+
+
+    def fastSP(self, node):
+        # use FastQueue with heap implementation
+        pass
+
 
 graph = Graph()
 
 graph.importFromFile("random.dot")
 graph.printTree()
 
+'''
+Q1) The first way, a slow way, to implement the queue is, as mentioned within the lab slides, to implement a simple queue without
+any special properties. In other words, a queue implemented as a linked list with tail enqueue and head dequeue. However, 
+searching through this queue would be O(n) due to the nature of it being completely unsorted (as it has no special
+properties). However, the second way to implement the queue is to implement it as a priority queue using a heap. 
+Since we are looking for the smallest current distance relative to the "origin" node, we can implement a 
+min-heap, where the node with the smallest distance is simply the node at the very top of the priority queue.
+Such an implementation would be O(1), since the node with the smallest distance is simply at the top of the queue.
+'''
