8cc08064c6832ee83331976d28225296b7b80ea9
Matthew McDougall <burke.mcdougall@ucalgary.ca>
2024-03-31 11:15:16 -0600

    part 3

--- ex3.py
+++ ex3.py
@@ -1,66 +1,135 @@
-import networkx as nx
-import matplotlib.pyplot as plt
+class Node:
 
-G = nx.Graph()
-G.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8, 9])
-edges_with_weights = [(1, 6, {'weight': 3}), (1, 7, {'weight': 2}), (2, 3, {'weight': 5}), 
-                      (2, 4, {'weight': 1}), (4, 7, {'weight': 4}), (4, 9, {'weight': 7}), 
-                      (5, 6, {'weight': 6}), (7, 8, {'weight': 3}), (4, 1, {'weight': 8})]
+    def __init__(self, data):
+        self.data = data
+        self.adjacent = []
 
-G.add_edges_from(edges_with_weights)
 
-pos = nx.spring_layout(G)
-nx.draw(G, pos, with_labels=True, node_color='lightblue', font_weight='bold')
-edge_labels = nx.get_edge_attributes(G, 'weight')
-nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
-plt.title("Original Graph")
-plt.show()
-
-def find(parent, i):
-    if parent[i] == i:
-        return i
-    return find(parent, parent[i])
-
-def union(parent, rank, x, y):
-    xroot = find(parent, x)
-    yroot = find(parent, y)
-
-    if rank[xroot] < rank[yroot]:
-        parent[xroot] = yroot
-    elif rank[xroot] > rank[yroot]:
-        parent[yroot] = xroot
-    else:
-        parent[yroot] = xroot
-        rank[xroot] += 1
-
-def kruskal(graph):
-    result = []
-    parent = {}
-    rank = {}
+class Edge:
     
-    edges = sorted(graph.edges(data=True), key=lambda x: x[2]['weight'])
-    
-    for node in graph.nodes():
-        parent[node] = node
-        rank[node] = 0
-    
-    for edge in edges:
-        u, v, weight = edge
-        u_parent = find(parent, u)
-        v_parent = find(parent, v)
-        
-        if u_parent != v_parent:
-            result.append((u, v, weight['weight']))
-            union(parent, rank, u_parent, v_parent)
-    
-    return result
+    def __init__(self, n1, n2, weight):
+        self.nodeOne = n1
+        self.nodeTwo = n2
+        self.weight = weight
 
-# Applying Kruskal's algorithm on the provided graph
-mst_edges = kruskal(G)
+    def __lt__(self, other):
+        return self.weight < other.weight
+    
+    def __eq__(self, other):
+        return (self.nodeOne == other.nodeTwo and self.nodeOne == other.nodeTwo)
 
-# Drawing the graph with MST edges highlighted
-nx.draw(G, pos, with_labels=True, node_color='lightblue', font_weight='bold')
-nx.draw_networkx_edges(G, pos, edgelist=mst_edges, edge_color='red', width=2)
-nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
-plt.title("Minimum Spanning Tree")
-plt.show()
+
+class Graph:
+
+    def __init__(self):
+        self.elements = []
+
+    def addNode(self, data):
+        node = Node(data)
+        self.elements.append(node)
+        return node
+
+    def removeNode(self, key):
+        for node in self.elements:
+            if node.data == key:
+                self.elements.remove(node) # this looks stupid but removeNode takes a key so i think i have to do it
+
+    def addEdge(self, n1, n2, weight):
+        edge = Edge(n1, n2, weight)
+        added1 = False
+        added2 = False # for checking successful adding of edge
+        for node in self.elements:
+            if node.data == n1.data:
+                node.adjacent.append(edge)
+                added1 = True
+            elif node.data == n2.data:
+                node.adjacent.append(edge)
+                added2 = True
+        if added1 == False or added2 == False: # this function works assuming n1 and n2 are already within the graph
+            print("Error adding edge")
+
+    def removeEdge(self, n1, n2):
+        for node in self.elements:
+            for edge in node.adjacent:
+                if edge.nodeOne == n1 and edge.nodeTwo == n2:
+                    node.adjacent.remove(edge) # removing the edge removes node 2
+
+    def importFromFile(self, file_path):
+        self.elements = []
+
+        with open(file_path, 'r') as f:
+            lines = f.readlines() 
+
+        if "strict graph" not in lines[0]:
+                print("not in")
+                return None 
+            
+
+        for line in lines[1:]:
+            line = line.strip()
+            if line == '}':
+                break
+
+            parts = line.replace('--', ' ').replace('[weight=', ' ').replace('];', ' ').split()
+            if len(parts) == 3:
+                node1_data, node2_data, weight = parts[0], parts[1], parts[2]
+            elif len(parts) == 2:
+                node1_data, node2_data, weight = parts[0], parts[1], 1
+            if len(parts) == 2 or len(parts) == 3:
+
+                node1 = self.findNode(node1_data)
+                if not node1:
+                    node1 = self.addNode(node1_data)
+                
+                node2 = self.findNode(node2_data)
+                if not node2:
+                    node2 = self.addNode(node2_data)
+
+                self.addEdge(node1, node2, int(weight))
+
+    def findNode(self, data):
+        for node in self.elements:
+            if node.data == data:
+                return node
+        return None
+    
+    def printTree(self):
+        for element in self.elements:
+            print(f"data: {element.data}")
+            for adj in element.adjacent:
+                print(f"adjacenies: {adj.nodeOne.data}, {adj.nodeTwo.data}, {adj.weight}")
+
+    def union_find(self, parent, node):
+        if parent[node] == node:
+            return node
+        return self.union_find(parent, parent[node])
+
+    def kruskal(self):
+        result = Graph()
+        parent = {}
+        for node in self.elements:
+            result.addNode(node.data)
+            parent[node.data] = node.data
+
+        sorted_edges = sorted((edge.weight, edge) for node in self.elements for edge in node.adjacent)
+        unique_edges = []
+
+        for edge in sorted_edges:
+            if edge not in unique_edges:
+                unique_edges.append(edge)
+
+        for weight, edge in unique_edges:
+            parent1 = self.union_find(parent, edge.nodeOne.data)
+            parent2 = self.union_find(parent, edge.nodeTwo.data)
+
+            if parent1 != parent2:
+                result.addEdge(edge.nodeOne, edge.nodeTwo, weight)
+                parent[parent1] = parent2
+
+        return result
+
+
+G = Graph()
+G.importFromFile("random.dot")
+MST = G.kruskal()
+MST.printTree()
